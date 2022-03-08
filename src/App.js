c891c8038400d16ec5be62a9e2b021a624f44c2e
Jason Kartz <jasonkartz@gmail.com>
2022-03-08 13:27:23 -0600

    chat window scroll to bottom on initial render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./index.css";
-import { db, auth } from "./backend/firebase-config";
+import { auth } from "./backend/firebase-config";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollections } from "react-firebase-hooks/firestore";
 import ChatRoom from "./components/ChatRoom";
 import SignIn from "./components/SignIn";
 import SignOut from "./components/SignOut";
 
 function App() {
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
   return (
     <div className="container mx-auto rounded drop-shadow-xl sm:w-5/6 md:w-4/6 lg:w-3/6 h-[95vh] flex flex-col">
       <nav className="flex justify-between p-2 bg-blue-100 bg-opacity-25 rounded-t md:text-lg">
         <h1 className="flex gap-2 text-2xl italic font-black tracking-wide text-blue-100 md:text-3xl">
-        <i class="ri-cloud-fill"></i> Cloud Chat
+          <i className="ri-cloud-fill"></i> Cloud Chat
         </h1>
         <SignOut user={user} />
       </nav>
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -3,7 +3,8 @@ import { signOut } from "firebase/auth";
 
 export default function SignOut({user}) {
   return user && (
-    <button className="px-2 py-1 font-bold text-blue-100 bg-blue-500 rounded drop-shadow hover:text-blue-50 active:drop-shadow-sm"
+    <button className="px-2 py-1 font-bold text-blue-100 bg-blue-500 
+    rounded drop-shadow hover:text-blue-50 active:drop-shadow-sm"
     onClick={() => signOut(auth)}
     >
       Sign Out
