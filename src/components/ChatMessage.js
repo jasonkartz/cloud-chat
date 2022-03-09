2ea46356621620a8816526698e521c5e12720cb2
Jason Kartz <jasonkartz@gmail.com>
2022-03-08 22:05:54 -0600

    ui updates

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,18 +1,26 @@
 import defaultPic from "../images/cloud-fill.png";
 import { auth } from "../backend/firebase-config";
 
-
 export default function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
+  const { text, uid, photoURL, displayName } = props.message;
 
+  const messageClass =
+    uid === auth.currentUser.uid ? "flex-row-reverse self-end" : "";
 
-  const messageClass = uid === auth.currentUser.uid ? "flex-row-reverse self-end" : "";
-  
   return (
     <>
-      <div className={`flex items-center gap-1 ${messageClass}`}>
-        <img src={photoURL || defaultPic} alt="user" width="45" className="rounded-full" />
-        <p className="p-2 bg-blue-100 rounded-full">{text}</p>
+      <div className={`flex items-end gap-1 ${messageClass}`}>
+        <img
+          src={photoURL || defaultPic}
+          alt="user"
+          width="45"
+          className="rounded"
+        />
+        <div className="flex flex-col items-end">
+          <p className="text-blue-800">{displayName || ""}</p>
+
+          <p className="p-1 bg-blue-100 rounded-lg text-slate-900">{text}</p>
+        </div>
       </div>
     </>
   );
--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -28,13 +28,14 @@ export default function ChatRoom() {
   const sendMessage = async (e) => {
     e.preventDefault();
 
-    const { uid, photoURL } = auth.currentUser;
+    const { uid, photoURL, displayName } = auth.currentUser;
 
     await addDoc(messagesRef, {
       text: formValue,
       createdAt: serverTimestamp(),
       uid,
       photoURL,
+      displayName,
     });
 
     setFormValue("");
@@ -42,29 +43,22 @@ export default function ChatRoom() {
 
   return (
     <>
-      <main className="flex flex-col gap-3 px-3 py-2 overflow-y-scroll bg-blue-300 grow">
+      <main className="chat-box">
         {messages &&
           messages.map((msg, index) => (
             <ChatMessage key={index} message={msg} />
           ))}
         <div ref={dummy}></div>
       </main>
-      <form
-        onSubmit={sendMessage}
-        className="flex items-center justify-between gap-2 p-2 bg-blue-100 bg-opacity-25 rounded-b md:text-lg"
-      >
+      <form onSubmit={sendMessage} className="message-form">
         <input
-          className="h-12 p-1 rounded shadow-inner text-slate-800 grow bg-blue-50"
+          type="text"
+          className="message-input"
           placeholder="Say something nice"
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
         ></input>
-        <button
-          type="submit"
-          className="w-12 h-12 font-bold text-blue-100 bg-blue-500 rounded 
-          disabled:opacity-50 drop-shadow hover:text-yellow-100 active:drop-shadow-sm"
-          disabled={!formValue}
-        >
+        <button type="submit" className="message-btn" disabled={!formValue}>
           <i className="ri-send-plane-fill"></i>
         </button>
       </form>
