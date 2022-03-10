96e0bea596f5a55df69ecb0737e497bcc1aa1a0b
Jason Kartz <jasonkartz@gmail.com>
2022-03-09 21:26:37 -0600

    toggle settings menu

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -9,17 +9,17 @@ export default function ChatMessage(props) {
 
   return (
     <>
-      <div className={`flex items-end gap-1 ${messageClass}`}>
+      <div className={`flex items-center gap-1 ${messageClass}`}>
         <img
           src={photoURL || defaultPic}
           alt="user"
           width="45"
           className="rounded"
         />
-        <div className="flex flex-col items-end">
-          <p className="text-blue-800">{displayName || ""}</p>
+        <div className={`flex flex-col p-1 gap-1 ${uid === auth.currentUser.uid ? "items-end":"items-start"}`}>
+          <p className="text-blue-900 text-xs">{displayName || ""}</p>
 
-          <p className="p-1 bg-blue-100 rounded-lg text-slate-900">{text}</p>
+          <p className="text-slate-900 bg-blue-100 p-1 rounded-lg">{text}</p>
         </div>
       </div>
     </>
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -5,9 +5,6 @@ export default function SignOut({ user }) {
   return (
     user && (
       <>
-        <p className="flex items-center gap-1 font-bold text-yellow-100 text-md">
-          <i class="ri-user-3-fill text-sm"></i> {user.displayName}
-        </p>
         <button className="signout-btn" onClick={() => signOut(auth)}>
           Sign Out
         </button>
