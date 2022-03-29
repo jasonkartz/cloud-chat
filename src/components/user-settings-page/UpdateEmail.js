c926098ffd698d9a72fabb4c9b05ca108d2bd435
Jason Kartz <jasonkartz@gmail.com>
2022-03-28 22:15:04 -0500

    broke up settings page into components

---
+++ src/components/user-settings-page/UpdateEmail.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { auth } from "../../backend/firebase-config";
+import { updateEmail } from "firebase/auth";
+import { updateDoc } from "firebase/firestore";
+
+export default function UpdateEmail(props) {
+  const currentUser = auth.currentUser;
+  const { email } = currentUser;
+  const accountRef = props.accountRef;
+
+  const [display, setDisplay] = useState(false);
+
+  const [emailForm, setEmailForm] = useState("");
+  const [emailStatus, setEmailStatus] = useState("");
+  const [passwordForm, setPasswordForm] = useState("");
+
+  const emailSubmit = async () => {
+    setEmailStatus("Updating email...");
+
+    await props.reauthenticate(passwordForm);
+
+    await updateEmail(currentUser, emailForm)
+      .then(() => {
+        updateDoc(accountRef, { email: emailForm }).then(() => {
+          setEmailStatus("Email updated: " + emailForm);
+          setEmailForm("");
+          setPasswordForm("");
+          setTimeout(() => setEmailStatus(""), 5000);
+        });
+      })
+      .catch((error) => {
+        setEmailStatus("Error updating email. Please try again.");
+        setEmailForm("");
+        setPasswordForm("");
+        setTimeout(() => setEmailStatus(""), 5000);
+      });
+  };
+
+  return (
+    <section className="border-b-2 border-blue-200 settings-section">
+      <h2
+        className="blue-heading hover:cursor-pointer hover:text-blue-500"
+        onClick={() => setDisplay(!display)}
+      >
+        Email{" "}
+        <i
+          className={`ri-arrow-${display ? "up" : "down"}-s-line align-sub`}
+        ></i>
+      </h2>
+      {display && (
+        <>
+          <input
+            type="email"
+            placeholder={email}
+            className="form-input"
+            value={emailForm}
+            onChange={(e) => {
+              setEmailForm(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="form-input"
+            value={passwordForm}
+            onChange={(e) =>
+              setPasswordForm(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="btn"
+            disabled={!emailForm || !passwordForm}
+            onClick={emailSubmit}
+          >
+            Change Email
+          </button>
+          <p>{emailStatus}</p>
+        </>
+      )}
+    </section>
+  );
+}
---
+++ src/components/user-settings-page/UpdateProfileImage.js
@@ -0,0 +1,114 @@
+import defaultPic from "../../images/cloud-fill.png";
+import { useState } from "react";
+import { storage, auth } from "../../backend/firebase-config";
+import { updateProfile } from "firebase/auth";
+import { updateDoc } from "firebase/firestore";
+import { uploadBytesResumable, ref, getDownloadURL } from "firebase/storage";
+
+export default function UpdateProfileImage(props) {
+  const currentUser = auth.currentUser;
+  const { uid } = currentUser;
+  const accountRef = props.accountRef;
+  const account = props.account;
+
+  const [display, setDisplay] = useState(false);
+
+  const types = ["image/jpeg", "image/png"];
+  const [file, setFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const handleImage = (e) => {
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setUploadStatus("Click 'Upload Image'.");
+    } else {
+      setFile(null);
+      setUploadStatus("Please select an image file (png or jpeg)");
+    }
+  };
+
+  const imageSubmit = () => {
+    setUploadStatus("Updating image...");
+    const imagePath =
+      "users/" + uid + "/user-image." + file.name.split(".").pop();
+    let fileRef = ref(storage, imagePath);
+    const uploadRef = uploadBytesResumable(fileRef, file);
+
+    uploadRef.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+        setUploadStatus("uploading");
+      },
+      () => (error) => {
+        setUploadStatus("upload error");
+      },
+      async () => {
+        const url = await getDownloadURL(fileRef);
+
+        await updateProfile(currentUser, { photoURL: url })
+          .then(() => {
+            setUploadStatus("complete");
+            updateDoc(accountRef, { photoURL: url });
+            setFile(null);
+            setTimeout(() => setUploadStatus(""), 5000);
+          })
+          .catch((error) => {
+            setUploadStatus("error");
+            setFile(null);
+          });
+      }
+    );
+  };
+
+  return (
+    <section className="border-b-2 border-blue-200 settings-section">
+      <h2 className="blue-heading hover:cursor-pointer hover:text-blue-500" onClick={() => setDisplay(!display)}>
+        Profile Image <i className={`ri-arrow-${display ? "up" : "down"}-s-line align-sub`}></i>
+      </h2>
+      {display && (
+        <>
+          <img
+            src={account.photoURL || defaultPic}
+            alt="user"
+            width="100"
+            className="rounded"
+          />
+
+          <input type="file" onChange={handleImage} />
+          {file && <div>File selected: {file.name}</div>}
+          <button
+            type="submit"
+            className={`btn`}
+            disabled={!file || uploadStatus === "uploading"}
+            onClick={imageSubmit}
+          >
+            {uploadStatus === "uploading" ? (
+              <div className="flex gap-1">
+                Uploading
+                <div className="animate-spin">
+                  <i className="ri-loader-5-line"></i>
+                </div>
+              </div>
+            ) : (
+              "Upload Image"
+            )}
+          </button>
+
+          {uploadStatus === "uploading"
+            ? "Uploading... " + uploadProgress + "%"
+            : uploadStatus === "complete"
+            ? "Image updated!"
+            : uploadStatus === "error"
+            ? "An error occured"
+            : ""}
+        </>
+      )}
+    </section>
+  );
+}
