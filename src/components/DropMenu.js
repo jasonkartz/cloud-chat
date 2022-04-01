184be8b2b44781006a86440a078f364aa859d704
Jason Kartz <jasonkartz@gmail.com>
2022-04-01 11:00:47 -0500

    refactored dropdown / setting up chatroom list

--- src/components/DropMenu.js
+++ src/components/DropMenu.js
@@ -11,65 +11,69 @@ export default function DropMenu({
   return (
     user && (
       <>
-        <nav className={`flex flex-col items-end select-none`}>
-          <ul
-            className={`drop-menu .visibility-transition ${
+        <nav className={`select-none`}>
+          <div
+            className={`drop-menu flex justify-between visibility-transition ${
               openMenu ? "m-fadeIn" : "m-fadeOut"
             }`}
           >
-            <li>
-              <button
-                className="menu-btn"
-                onClick={() => {
-                  setScreen(screenMap.settings);
-                  setOpenMenu(false);
-                }}
-              >
-                <i className="ri-user-settings-line"></i> User Settings
-              </button>
-            </li>
-            <li>
-              <button
-                className="menu-btn"
-                onClick={() => {
-                  setScreen(screenMap.chat);
-                  setOpenMenu(false);
-                }}
-              >
-                <i className="ri-search-line"></i> Public Chats
-              </button>
-            </li>
-            <li>
-              <button
-                className="menu-btn"
-                onClick={() => setOpenMenu(false)}
-              >
-                <i className="ri-chat-private-line"></i> Direct Chat
-              </button>
-            </li>
-            <li>
-              <button
-                className="menu-btn"
-                onClick={() => setOpenMenu(false)}
-              >
-                <i className="ri-chat-new-line"></i> Create Chat
-              </button>
-            </li>
-            <li>
-              <button
-                className="menu-btn"
-                onClick={() => {
-                  signOut(auth);
-                  setOpenMenu(false);
-                  setScreen(screenMap.chat);
-                }}
-              >
-                <i className="align-bottom ri-logout-box-line"></i> Sign Out
-              </button>
-            </li>
-          </ul>
+            <ul>
+              <li>Chatroom 1</li>
+              <li>Chatroom 2</li>
+              <li>Chatroom 3</li>
+            </ul>
+
+            <ul className="text-right ">
+              <li>
+                <button
+                  className="menu-btn"
+                  onClick={() => {
+                    setScreen(screenMap.settings);
+                    setOpenMenu(false);
+                  }}
+                >
+                  <i className="ri-user-settings-line"></i> User Settings
+                </button>
+              </li>
+              <li>
+                <button
+                  className="menu-btn"
+                  onClick={() => {
+                    setScreen(screenMap.chat);
+                    setOpenMenu(false);
+                  }}
+                >
+                  <i className="ri-search-line"></i> Public Chats
+                </button>
+              </li>
+              <li>
+                <button className="menu-btn" onClick={() => setOpenMenu(false)}>
+                  <i className="ri-chat-private-line"></i> Direct Chat
+                </button>
+              </li>
+              <li>
+                <button className="menu-btn" onClick={() => setOpenMenu(false)}>
+                  <i className="ri-chat-new-line"></i> Create Chat
+                </button>
+              </li>
+              <li>
+                <button
+                  className="menu-btn"
+                  onClick={() => {
+                    signOut(auth);
+                    setOpenMenu(false);
+                    setScreen(screenMap.chat);
+                  }}
+                >
+                  <i className="align-bottom ri-logout-box-line"></i> Sign Out
+                </button>
+              </li>
+            </ul>
+          </div>
         </nav>
       </>
     )
   );
 }
+
+/*flex flex-col items-end */
